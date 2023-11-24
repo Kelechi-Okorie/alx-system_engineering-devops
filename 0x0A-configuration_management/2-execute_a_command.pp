cb992efdb945522da16f1370ca83ccb7eeeddf4b
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-11-24 20:32:59 +0100

    Hot fix on task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # kills a process named killmenow
 exec { 'killmenow':
-  command  => 'user/bin/pkill killmenow',
+  command  => '/usr/bin/pkill killmenow',
   provider => 'shell',
   returns  => [0, 1],
 }
