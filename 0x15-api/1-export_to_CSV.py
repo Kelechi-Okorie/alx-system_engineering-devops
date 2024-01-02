eed9b20a39bfabad812ab5b39b4112ccc6b9b49e
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-01-02 16:24:10 +0100

    Added task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""For a given employee ID, returns information about
+his/her TODO list progress"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+
+    url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    response = requests.get(url)
+    user = response.json()
+    name = user.get('name')
+
+    url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url)
+    todos = response.json()
+
+    filename = id + '.csv'
+    with open(filename, mode="w") as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+
+        for task in todos:
+            if task.get('id') == int(id):
+                writer.writerow([id, name, str(task.get('completed')),
+                                 task.get('title ')])
