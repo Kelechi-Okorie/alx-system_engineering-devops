c8a36eeb58c594653bcf5b87f6cec02dd8aae4e7
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-01-27 17:42:54 +0000

    Added task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# make nginx to stop dropping requests
+exec {'fix':
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/sbin']
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,4 @@
+# Readme file for 0x1B-web_stack_debugging 4
+
+
+File 0-the_sky_is_the_limit_not.pp - A puppet manifest to fix nginx to make it stop dropping requests
