cb36f300892679edd62a183a1b080ac29a4f972d
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-01-27 17:48:09 +0000

    Bug fix to task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,11 @@
 # make nginx to stop dropping requests
 exec {'fix':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
-  path    => ['/bin', '/usr/sbin']
+  command => "sed -i "s/15/4096/" /etc/default/nginx,
+  path    => ['/bin', '/usr/sbin'],
+}
+
+# restart nginx
+exec {'restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d',
 }
