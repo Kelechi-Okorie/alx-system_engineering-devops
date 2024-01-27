e3fa4f61c40755f0d6ea0c0d504bb6ac51869af6
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-01-27 17:51:48 +0000

    Bug fix on task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # make nginx to stop dropping requests
 exec {'fix':
-  command => "sed -i "s/15/4096/" /etc/default/nginx,
+  command => "sed -i 's/15/4096/' /etc/default/nginx",
   path    => ['/bin', '/usr/sbin'],
 }
 
